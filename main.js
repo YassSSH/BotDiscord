475873367da2d60b447172c98379e934682ed568
unknown <yassine.regragui@live.fr>
2020-05-26 13:17:46 +0200

    Ajout d'options everyone + opti

---
+++ main.js
@@ -0,0 +1,14 @@
+const {Client} = require('discord.js');
+const client = new Client({disableMentions:"everyone"});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') msg.channel.send('Pong!');
+  if (msg.content === 'everyone') msg.channel.send('@everyone, wsh les moulagas', {disableMentions:"none"});
+  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), wsh les moulagas');
+});
+
+client.login('NzE0Nzc3NzM5ODc3Njc5MTU1.XsznFQ.K-EDb5u4EZhUX22DN-fr021IW0Y');
