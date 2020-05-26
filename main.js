9c88962948a120cba650c74d77226046018f0983
unknown <yassine.regragui@live.fr>
2020-05-26 17:25:44 +0200

    Ajout de plusieur commandes et changement TOKEN

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const {Client} = require('discord.js');
-const client = new Client({disableMentions:"everyone"});
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
+const client = new Client({ disableMentions: "off"});
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("J'suis pret mon boug");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send('@everyone, wsh les moulagas', {disableMentions:"none"});
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), wsh les moulagas');
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}wsh`)) msg.channel.send("bien ?");
+  if (msg.content.startsWith(`${PREFIX}stop`)) msg.channel.send("calme oit chacal");
+  if (msg.content.startsWith(`${PREFIX}oxah`)) msg.channel.send("La soeur a oxah j'la baise");
+  if (msg.content.startsWith(`${PREFIX}twitch`)) msg.channel.send("https://www.twitch.tv/");
+  if (msg.content.startsWith(`${PREFIX}mzx`)) msg.channel.send("BLACKIE LA PUTE");
+  if (msg.content.startsWith(`${PREFIX}loco`)) msg.channel.send("jtm");
+  if (msg.content.startsWith(`${PREFIX}all`)) msg.channel.send("@everyone Flaquito il est grave beau j'le suce");
 });
 
-client.login('NzE0Nzc3NzM5ODc3Njc5MTU1.Xsz7ow.YcAiLYYqIV7P6ftkOzUORsTkLyc');
+client.login(TOKEN);
