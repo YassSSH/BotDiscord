4d55c59bfada87dc625c7634d4c507ebb2f2063b
unknown <yassine.regragui@live.fr>
2020-05-28 12:17:19 +0200

    opti PREFIX

--- main.js
+++ main.js
@@ -5,28 +5,30 @@ const client = new Client({ disableMentions: "off" });
 
 client.on("message", msg => {
   if (msg.author.bot) return;
-  // --------------------------------
-  const args = msg.content.split(/ +/g);
+  if (msg.content.indexOf(PREFIX) !== 0) return;
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
-  if (cmd === `${PREFIX}defoncer`) msg.channel.send(" https://cdn.discordapp.com/attachments/653314571058020383/708043566546681986/quand_il_va_te_defonce.mp4");
-  if (cmd === `${PREFIX}sieste`) msg.channel.send("https://cdn.discordapp.com/attachments/694272443170160692/707743089061527602/ritchie_sieste.mp4");
-  if (cmd === `${PREFIX}quoi`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/706684763070791770/0RIXbtGmK7wnnd_v.mp4");
-  if (cmd === `${PREFIX}ritchi`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/706550754902802432/Snapchat-712967593.mp4");
-  if (cmd === `${PREFIX}r`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/705900764970614915/putainnn.mp4");
-  if (cmd === `${PREFIX}allo`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704643293572759632/video0.mov");
-  if (cmd === `${PREFIX}nrv`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704643262907941004/video0.mov");
-  if (cmd === `${PREFIX}velo`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704461053773480047/O5KGJaafGKalPTYL.mp4");
-  if (cmd === `${PREFIX}bombarde`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704417429811494912/on_y_va_point_.mp4");
-  if (cmd === `${PREFIX}leto`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704343177816965180/leto_petasse.mp4");
-  if (cmd === `${PREFIX}salva`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704343176093368450/tes_une_putain_de_pute.mp4");
-  if (cmd === `${PREFIX}chelou`) msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704163951885287464/video0.mov");
-  if (cmd === `${PREFIX}merci`) msg.channel.send("https://discordapp.com/channels/666081842683969557/669998446866071552/703677791454888087");
-  if (cmd === `${PREFIX}asmr`) msg.channel.send("https://www.youtube.com/watch?v=hSvTdqOiTpg");
-  if (cmd === `${PREFIX}repeat`) {
+  // --------------------------------COMMANDES+REPEAT
+  if (cmd === "defoncer") msg.channel.send(" https://cdn.discordapp.com/attachments/653314571058020383/708043566546681986/quand_il_va_te_defonce.mp4");
+  if (cmd === "sieste") msg.channel.send("https://cdn.discordapp.com/attachments/694272443170160692/707743089061527602/ritchie_sieste.mp4");
+  if (cmd === "quoi") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/706684763070791770/0RIXbtGmK7wnnd_v.mp4");
+  if (cmd === "ritchi") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/706550754902802432/Snapchat-712967593.mp4");
+  if (cmd === "r") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/705900764970614915/putainnn.mp4");
+  if (cmd === "allo") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704643293572759632/video0.mov");
+  if (cmd === "nrv") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704643262907941004/video0.mov");
+  if (cmd === "velo") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704461053773480047/O5KGJaafGKalPTYL.mp4");
+  if (cmd === "bombarde") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704417429811494912/on_y_va_point_.mp4");
+  if (cmd === "leto") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704343177816965180/leto_petasse.mp4");
+  if (cmd === "salva") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704343176093368450/tes_une_putain_de_pute.mp4");
+  if (cmd === "chelou") msg.channel.send("https://cdn.discordapp.com/attachments/669998446866071552/704163951885287464/video0.mov");
+  if (cmd === "merci") msg.channel.send("https://discordapp.com/channels/666081842683969557/669998446866071552/703677791454888087");
+  if (cmd === "test") msg.channel.send("test");
+  if (cmd === "repeat") {
     msg.channel.send(args.join(" "));
     msg.delete({ timeout: 0 }).then(msg => console.log(`Message deleted bouffon: ${msg.content}`));
   }
-  if (cmd === `${PREFIX}role`) {
+  // ---------------------------------------- ROLES
+  if (cmd === "role") {
     const channel = client.channels.cache.get("714779936849788929");
     const role = msg.guild.roles.cache.find(r => r.name === args[0]);
     if (!role) return msg.channel.send("Ce role n'existe pas mon gars");
@@ -41,7 +43,7 @@ client.on("message", msg => {
     }
   }
 });
-
+// ---------------------------BIENVENUE
 client.on("guildMemberAdd", member => {
   member.send("Bienvenue chez le meilleur dev");
   const channel = client.channels.cache.get("715492459030904892");
@@ -49,7 +51,7 @@ client.on("guildMemberAdd", member => {
 });
 
 client.login(TOKEN);
-// -----------------------------
+// -----------------------------CONSOLE,LOGS
 client.on("ready", () => console.log("je suis pret chakal"));
 client.on("error", () => console.error);
 client.on("warn", () => console.warn);
